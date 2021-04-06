e45f74be0c928dd51ef3b16e52fb20a7e3c2afc0
DiegoBrito <dibbrito@gmail.com>
2021-04-05 21:03:27 -0300

    Adc home component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { AcessoNegadoComponent } from './navegacao/acesso-negado/acesso-negado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AcessoNegadoComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/navegacao/acesso-negado/acesso-negado.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AcessoNegadoComponent } from './acesso-negado.component';
+
+describe('AcessoNegadoComponent', () => {
+  let component: AcessoNegadoComponent;
+  let fixture: ComponentFixture<AcessoNegadoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AcessoNegadoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AcessoNegadoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
