65c93b2ac58277972f1ffe5c77f2e73cc9b9d0f0
DiegoBrito <dibbrito@gmail.com>
2021-04-07 21:10:05 -0300

    Add buttons for Vant page

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { AcessoNegadoComponent } from './navegacao/acesso-negado/acesso-negado.component';
 import { VantComponent } from './navegacao/vant/vant.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { VantComponent } from './navegacao/vant/vant.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
