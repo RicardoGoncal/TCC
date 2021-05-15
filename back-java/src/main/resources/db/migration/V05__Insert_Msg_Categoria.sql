83e4e4f406587425bac341403bd1ac1c6cb29ade
ricardo-ubuntu <ricardo.goncalves7@fatec.sp.gov.br>
2021-05-15 18:32:20 -0300

    arquivo V05

---
+++ back-java/src/main/resources/db/migration/V05__Insert_Msg_Categoria.sql
@@ -0,0 +1,19 @@
+INSERT into mensagem
+(id_categoria, mensagem)
+values (1, 'CLIMB TO FL'),
+       (1, 'MAINTAIN FL'),
+       (1, 'CLIMB TO AND MAINTAIN FL'),
+       (2, 'ROUTE 666'),
+       (2, 'ROUTE 116'),
+       (3, 'DESCEND FL'),
+       (3, 'DESCEND TO AND MAINTAIN FL'),
+       (4, 'COMMS 1'),
+       (4, 'COMMS 2'),
+       (5, 'SPEED X'),
+       (5, 'MAINTAIN SPEED X'),
+       (6, 'REPORT 1'),
+       (6, 'REPORT 2'),
+       (7, 'EMERG 1'),
+       (7, 'EMERG 2'),
+       (8, 'CROSSING 1'),
+       (8, 'CROSSING 2');
