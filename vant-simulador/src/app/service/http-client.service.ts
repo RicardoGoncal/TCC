5957d5b84fcec70cd8626f1278c3c696d5660a23
Diego Brito <dibbrito@gmail.com>
2021-05-16 15:06:33 -0300

    initial commit for vant-simulator

---
+++ vant-simulador/src/app/service/http-client.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+export class Categoria{
+  constructor(
+    public id:string,
+    public nome:string,
+  ) {}
+}
+
+export class Vants{
+  constructor(
+    public id: string,
+    public nome: string,
+  ) { }
+}
+
+export class Mensagem{
+  constructor(
+    public id: string,
+    public mensagem: string,
+  ) { }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  basic: any;
+
+  constructor( private httpClient: HttpClient) {  }
+
+  getCategorias() {
+    
+    this.basic = sessionStorage.getItem('basicauth')
+     
+    let headers = new HttpHeaders({ Authorization: this.basic });
+    return this.httpClient.get<Categoria>('http://localhost:8080/categorias', { headers })
+    
+  }
+
+  getVants() {
+    this.basic = sessionStorage.getItem('basicauth')
+    let headers = new HttpHeaders({ Authorization: this.basic });
+    return this.httpClient.get<Vants[]>('http://localhost:8080/vants', { headers })
+    .pipe(
+      tap(vants => console.log('leu os vants' + vants)),
+      catchError(this.handlerError('getCliente', []))
+    );
+  }
+
+  private handlerError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error)
+      return of(result as T)
+    }
+  }
+
+  getMensagem(type: string){
+
+    let url = `http://localhost:8080/mensagens/${type}`
+    
+    this.basic = sessionStorage.getItem('basicauth')
+    let headers = new HttpHeaders({ Authorization: this.basic });
+    return this.httpClient.get<Mensagem[]>(url, { headers })
+    .pipe(
+      tap(mensagens => console.log('leu as mensagens' + mensagens)),
+      catchError(this.handlerError('getMensagem', []))
+    )
+  }
+
+}
