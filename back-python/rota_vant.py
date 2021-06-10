c745680b90edfa480d0f375a6efb7a2b749a8277
ricardo-ubuntu <ricardo.goncalves7@fatec.sp.gov.br>
2021-06-10 20:47:33 -0300

    Atualizacao rota_python

--- back-python/rota_vant.py
+++ back-python/rota_vant.py
@@ -4,31 +4,23 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
-import mysql.connector
+from tarefas_bd import Tarefas_Bd
 import datetime
 
-
-# Realizar a inserção de dados na tabela de LOG
-def inserir_log(id_vant, msg_vant):
-    mydb = mysql.connector.connect(
-        host = 'localhost',
-        user = 'tcc',
-        password = 'Seformar@21',
-        database = 'cpdlc2'
-    )
-
-    data = datetime.datetime.now()
-
-    mycursor = mydb.cursor()
-    query = "insert into log_msg (id_vant, mensagem, data_envio) values('{}','{}','{}')".format(id_vant,msg_vant,data)
-    mycursor.execute(query)
-
-    mydb.commit()
-    mydb.close()
-
-# Criação da API 
+# Configuração da API
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Area das funções
+def log_torre(id_vant, msg_vant):
+    """
+        Realizar a inserção de dados na tabela de log da Torre 
+        de comando
+    """
+    log_torre = Tarefas_Bd()
+    data = datetime.datetime.now()
+
+    log_torre.inserir_log_torre(id_vant=id_vant, msg_vant=msg_vant, data=data)
 
 @app.route('/vant', methods=['POST','GET'])
 def vant():
@@ -40,18 +32,13 @@ def vant():
     if request.json is None:
         return jsonify({"mensagem":"ERRO: Mensagem invalida ou nula"})
     else:
-        content = {"mensagem":"Mensagem recebida"}
+        content = {}
         content.update(request.json)
-
-        # Realizar a separação do json que virá do front
-        #TODO trocar pelas infos corretas após atualização no envio do front
-        id_vant = str('2')
-        msg_vant = 'alter route to 890;report to pilot'
+        print(content)
 
         # Inserir na tabela de log no BD
-        inserir_log(id_vant=id_vant, msg_vant=msg_vant)
+        log_torre(id_vant=content['vant'], msg_vant=content['message'])
 
         # coelho
 
-        print(content)
         return jsonify(content)
