3f88332d645ae9fcaf638451d2926a58861b0e5b
ricardo-ubuntu <ricardo.goncalves7@fatec.sp.gov.br>
2021-06-09 23:30:35 -0300

    Atualizacao rota_vant - envio log

--- back-python/rota_vant.py
+++ back-python/rota_vant.py
@@ -4,8 +4,29 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+import mysql.connector
+import datetime
 
-# nome da api
+
+# Realizar a inserção de dados na tabela de LOG
+def inserir_log(id_vant, msg_vant):
+    mydb = mysql.connector.connect(
+        host = 'localhost',
+        user = 'tcc',
+        password = 'Seformar@21',
+        database = 'cpdlc2'
+    )
+
+    data = datetime.datetime.now()
+
+    mycursor = mydb.cursor()
+    query = "insert into log_msg (id_vant, mensagem, data_envio) values('{}','{}','{}')".format(id_vant,msg_vant,data)
+    mycursor.execute(query)
+
+    mydb.commit()
+    mydb.close()
+
+# Criação da API 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -21,4 +42,16 @@ def vant():
     else:
         content = {"mensagem":"Mensagem recebida"}
         content.update(request.json)
+
+        # Realizar a separação do json que virá do front
+        #TODO trocar pelas infos corretas após atualização no envio do front
+        id_vant = str('2')
+        msg_vant = 'alter route to 890;report to pilot'
+
+        # Inserir na tabela de log no BD
+        inserir_log(id_vant=id_vant, msg_vant=msg_vant)
+
+        # coelho
+
+        print(content)
         return jsonify(content)
